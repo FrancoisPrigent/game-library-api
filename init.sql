ae91c667459c6f90f49429f9c97821ac546fc1c4
François Prigent <francois@prigent.xyz>
2021-07-27 20:33:50 +0200

    init database schema and datas

---
+++ init.sql
@@ -0,0 +1,74 @@
+/*************************************/
+/* TABLE CREATION */
+/*************************************/
+CREATE TABLE IF NOT EXISTS publisher
+(
+  id    SERIAL,
+  name  VARCHAR(100)  NOT NULL,
+  siret INT           NOT NULL,
+  phone VARCHAR(20)   NOT NULL,
+
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS game
+(
+  id            SERIAL,
+  title         VARCHAR(100)  NOT NULL,
+  price         INT           NOT NULL,
+  publisher_id  BIGINT        UNSIGNED    NOT NULL,
+  releaseDate   TIMESTAMP     NOT NULL,
+
+  PRIMARY KEY (id),
+  FOREIGN KEY (publisher_id) REFERENCES publisher(id)
+);
+
+CREATE TABLE IF NOT EXISTS tag
+(
+  id    SERIAL,
+  name  VARCHAR(50)   NOT NULL,
+
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS game_tag
+(
+  id        SERIAL,
+  game_id   BIGINT  UNSIGNED    NOT NULL,
+  tag_id    BIGINT  UNSIGNED    NOT NULL,
+
+  PRIMARY KEY (id),
+  FOREIGN KEY (game_id) REFERENCES game(id),
+  FOREIGN KEY (tag_id) REFERENCES tag(id)
+);
+
+/*************************************/
+/* DATA INSERTION */
+/*************************************/
+INSERT INTO publisher
+  (name, siret, phone)
+VALUES
+  ('ubisoft', 99999, '0101010101'),
+  ('riot', 88888, '0202020202');
+
+INSERT INTO game
+  (title, price, publisher_id, releaseDate)
+VALUES
+  ('overwatch', 50, 1, '2021-01-01 00:00:01'),
+  ('league of legends', 100, 2, '2021-02-01 00:00:01');
+
+INSERT INTO tag
+  (name)
+VALUES
+  ('fight'),
+  ('team'),
+  ('competition');
+
+INSERT INTO game_tag
+  (game_id, tag_id)
+VALUES
+  (1, 1),
+  (1, 2),
+  (2, 1),
+  (2, 2),
+  (2, 3);
