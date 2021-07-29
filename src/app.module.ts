e6d0bad12dcd8fb750e4813e900d21e20dc517ed
François Prigent <francois@prigent.xyz>
2021-07-29 12:47:11 +0200

    database connection initialization with module import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getConnectionOptions, Connection } from 'typeorm';
+import { AppController } from './app.controller';
 import { GamesModule } from './games/games.module';
 
 @Module({
-  imports: [GamesModule],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: async () =>
+        Object.assign(await getConnectionOptions(), {
+          autoLoadEntities: true,
+        }),
+    }),
+    GamesModule,
+  ],
+  controllers: [AppController],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private connection: Connection) {}
+}
