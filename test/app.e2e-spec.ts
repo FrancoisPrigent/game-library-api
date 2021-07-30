3c187572b88ae9afabf00a54ad65cd22c8de43aa
François Prigent <francois@prigent.xyz>
2021-07-30 18:27:53 +0200

    add health check test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -17,8 +17,12 @@ describe('AppController (e2e)', () => {
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/health-check')
       .expect(200)
-      .expect('Hello World!');
+      .expect('ok');
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
